559f293c046ffb3c55236f83e52fb039ba270082
aguzmant103 <67167307+aguzmant103@users.noreply.github.com>
2022-11-03 15:14:25 +0000

    Adding Buyer Class

--- types & classes/types&classes.js
+++ types & classes/types&classes.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Seller = exports.Marketplace = void 0;
+exports.Buyer = exports.Seller = exports.Marketplace = void 0;
 class Marketplace {
-}
+} // Pending to Implement
 exports.Marketplace = Marketplace;
 class User {
     constructor(username) {
@@ -10,12 +10,14 @@ class User {
     }
 }
 class Seller extends User {
+    // Constructor
     constructor(userName) {
         super(userName);
         this.userName = userName;
         this.myListings = [];
         this.totalSold = 0;
     }
+    // Methods
     getTotalSold() {
         return this.totalSold;
     }
@@ -24,3 +26,20 @@ class Seller extends User {
     }
 }
 exports.Seller = Seller;
+class Buyer extends User {
+    // Constructor
+    constructor(userName) {
+        super(userName);
+        this.userName = userName;
+        this.myBids = [];
+        this.totalSpent = 0;
+    }
+    // Methods
+    getTotalSpent() {
+        return this.totalSpent;
+    }
+    getMyBids() {
+        return this.myBids;
+    }
+}
+exports.Buyer = Buyer;
--- types & classes/types&classes.ts
+++ types & classes/types&classes.ts
@@ -1,27 +1,48 @@
-export class Marketplace {
-
-}
-
+export class Marketplace {} // Pending to Implement
 abstract class User {
     constructor (public username : string){}
 }
-
 export class Seller extends User{
 
-    myListings : [];
+    // Properties
+    myListings : []; // PENDING NEED TO IMPLEMENT LISTING TYPE
     totalSold : number;
 
+    // Constructor
     constructor (public userName : string) {
         super(userName);
         this.myListings = [];
         this.totalSold = 0;
     }
 
+    // Methods
     getTotalSold(){
         return this.totalSold;
     }
 
     getMyListings(){
         return this.myListings;
+    }
+}
+export class Buyer extends User{
+
+    // Properties
+    myBids : []; // PENDING NEED TO IMPLEMENT BIDS TYPE
+    totalSpent : number;
+
+    // Constructor
+    constructor (public userName : string) {
+        super(userName);
+        this.myBids = [];
+        this.totalSpent = 0;
+    }
+
+    // Methods
+    getTotalSpent(){
+        return this.totalSpent;
+    }
+
+    getMyBids(){
+        return this.myBids;
     }
 }
